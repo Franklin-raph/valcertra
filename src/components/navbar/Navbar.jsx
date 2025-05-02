83927835d7df12be8050f9a9cdef2fe50a0f6532
franklin-raph <igboekwulusifranklin@gmail.com>
2025-05-02 23:03:17 +0100

    fixed the nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [useCustomUI, setUseCustomUI] = useState(false);
+  const [openNav, setOpeNav] = useState(false);
 
   // <div className="flex items-center space-x-4">
   //   <div>
@@ -19,9 +20,40 @@ const Navbar = () => {
 
   return (
     <div>
-      <div className="bg-primary-color shadow rounded-lg px-4 py-4 mb-6 flex items-center justify-between w-[95%] mx-auto">
-        <p className='text-secondary-color'>Logo</p>
-        <ul className='flex items-center gap-[20px]'>
+      <div className="bg-primary-color shadow sm:rounded-lg px-4 py-4 mb-6 flex items-center justify-between sm:w-[95%] mx-auto">
+        <Link to="/" className="text-secondary-color text-[20px] font-[600]">Logo</Link>
+        <img src="./open-nav.svg" className='cursor-pointer block sm:hidden' alt="" onClick={() => setOpeNav(true)}/>
+        {
+          openNav &&
+          <div className='fixed top-0 right-0 w-full h-screen bg-[#00000087] z-50'>
+            <div className='flex items-center justify-between bg-white w-full px-4 py-4 mt-7'>
+              <Link to="/" className="text-[20px] text-primary-color font-[600]">Logo</Link>
+              <img src="./close-nav.svg" className='cursor-pointer block sm:hidden' alt="" onClick={() => setOpeNav(false)}/>
+            </div>
+            <div className='bg-white w-full ml-auto pb-[2.5rem] pt-[1rem]'>
+              <ul className='flex flex-col items-left px-4 gap-[20px]'>
+                <li>
+                  <Link to="/" className="text-[#666666] hover:text-gray-300">Home</Link>
+                </li>
+                <li>
+                  <Link to="/services" className="text-[#666666] hover:text-gray-300">Services</Link>
+                </li>
+                <li>
+                  <Link to="/about-us" className="text-[#666666] hover:text-gray-300">About Us</Link>
+                </li>
+                <li>
+                  <Link to="/contact-us" className="text-[#666666] hover:text-gray-300">Contact Us</Link>
+                </li>
+              </ul>
+              <div className='flex flex-col items-left px-4 mt-[20px] gap-5'>
+                <GoogleTranslateElement />
+                <button className='bg-secondary-color px-[20px] py-[5px] rounded-[4px] w-[142px]'>Login</button>
+              </div>
+            </div>
+          </div>
+        }
+
+        <ul className='hidden sm:flex items-center gap-[20px]'>
           <li>
             <Link to="/" className="text-white hover:text-gray-300">Home</Link>
           </li>
@@ -35,7 +67,7 @@ const Navbar = () => {
             <Link to="/contact-us" className="text-white hover:text-gray-300">Contact Us</Link>
           </li>
         </ul>
-        <div className='flex items-center gap-5'>
+        <div className='hidden sm:flex items-center gap-5'>
           <GoogleTranslateElement />
           <button className='bg-secondary-color px-[20px] py-[5px] rounded-[4px]'>Login</button>
         </div>
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -71,7 +71,7 @@ const About = () => {
   return (
     <div>
 
-      <section className='md:h-[70vh] pb-[2.5rem] header-bg'>
+      <section className='md:h-[70vh] pb-[2.5rem] pt-[2rem] header-bg'>
           <Navbar />
           <div className='text-center mt-[6rem] md:w-[65%] w-[95%] mx-auto  text-white'>
               <p className='lg:text-[55px] text-[30px] font-bold mb-2'> Who We <span className='text-ascent-color'>are</span> .</p>
